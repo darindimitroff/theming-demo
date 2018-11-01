e1ed0baa672edc843f04d22c3f32283d07b086b4
Darin Dimitroff <d@thecrazyones.agency>
2018-11-01 14:08:27 +0200

    Fix button

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -5,7 +5,7 @@ import Icon from 'components/atoms/icon'
 
 const Button = props => {
     return (
-        <Box {...props} is="button" display="flex" justifyContent="center" alignItems="center" border={1} p={1} color="grey85" bg="grey37" borderColor="grey21" borderRadius={1}>
+        <Box {...props} is="button" display="flex" justifyContent="center" alignItems="center" border={1} px={2} py={1} color="grey85" bg="grey37" borderColor="grey21" borderRadius={1}>
             {props.icon ?
                 <Icon mr={2} glyph={props.icon} />
                 : null}
