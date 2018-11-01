848e9a35656ac7b088cd6526ffc627a6ac8dc72c
Darin Dimitroff <d@thecrazyones.agency>
2018-11-01 14:08:33 +0200

    Fix input

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -6,15 +6,14 @@ import Text from 'components/primitives/text'
 const Input = props => {
     return (
         <Flex {...props} alignItems="center" position="relative" overflow="hidden">
-            <Box is="input" width={1} border={1} p={1} pr={8} color="grey85" bg="grey17" borderColor="grey10" borderRadius={1} />
+            <Box is="input" width={1} border={1} pl={2} py={1} pr={8} color="grey85" bg="grey17" borderColor="grey10" borderRadius={1} />
 
             {/* Render a unit only if one is passed */}
             {props.unit ?
-                <Flex position="absolute" right={0} top={0} height="100%" alignItems="center" p={1} pr={2} color="grey65" value={props.value} fontFamily="system">
+                <Flex position="absolute" zIndex="2" right={0} top={0} height="100%" alignItems="center" p={1} pr={2} color="grey65" value={props.value} fontFamily="system">
                     <Text fontSize={1} css={{ textTransform: 'uppercase' }}>{props.unit}</Text>
                 </Flex> : false
             }
-
         </Flex>
     )
 }
