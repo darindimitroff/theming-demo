eee2914532802c33427ed07bee0b04bd21db8ebf
Darin Dimitroff <d@spacefarm.digital>
2018-11-14 12:05:16 +0100

    Fix capitalized Flex-end

--- src/components/organisms/Screen/index.js
+++ src/components/organisms/Screen/index.js
@@ -3,7 +3,7 @@ import Flex from 'components/atoms/Flex'
 
 const Screen = props => {
     return (
-        <Flex width="100vw" height="100vh" justifyContent="Flex-end">{props.children}</Flex>
+        <Flex width="100vw" height="100vh" justifyContent="flex-end">{props.children}</Flex>
     )
 }
 
